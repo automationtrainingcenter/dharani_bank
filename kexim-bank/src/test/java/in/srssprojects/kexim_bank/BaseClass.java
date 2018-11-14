8aecc63a24a233ea2ca812da8254e69987ebaa37
automationtrainingcenter <atcsurya@gmail.com>
2018-11-14 18:19:47 +0530

    grid execution and final

--- kexim-bank/src/test/java/in/srssprojects/kexim_bank/BaseClass.java
+++ kexim-bank/src/test/java/in/srssprojects/kexim_bank/BaseClass.java
@@ -2,6 +2,8 @@ package in.srssprojects.kexim_bank;
 
 import java.io.File;
 import java.io.IOException;
+import java.net.MalformedURLException;
+import java.net.URL;
 import java.text.DateFormat;
 import java.text.SimpleDateFormat;
 import java.util.Date;
@@ -9,10 +11,14 @@ import java.util.concurrent.TimeUnit;
 
 import org.apache.commons.io.FileUtils;
 import org.openqa.selenium.OutputType;
+import org.openqa.selenium.Platform;
 import org.openqa.selenium.TakesScreenshot;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.chrome.ChromeDriver;
 import org.openqa.selenium.firefox.FirefoxDriver;
+import org.openqa.selenium.remote.BrowserType;
+import org.openqa.selenium.remote.DesiredCapabilities;
+import org.openqa.selenium.remote.RemoteWebDriver;
 import org.openqa.selenium.support.events.EventFiringWebDriver;
 
 import com.relevantcodes.extentreports.ExtentReports;
@@ -53,10 +59,10 @@ public class BaseClass {
 	
 	public void launchBrowser(String browserName, String url) throws InvalidBrowserNameException {
 		if(browserName.equalsIgnoreCase("chrome")) {
-			System.setProperty("webdriver.chrome.driver", ".\\drivers\\chromedriver.exe");
+			System.setProperty("webdriver.chrome.driver", getFilePath("drivers", "chromedriver.exe"));
 			driver = new ChromeDriver();
 		}else if(browserName.equalsIgnoreCase("firefox")) {
-			System.setProperty("webdriver.gecko.driver", ".\\drivers\\geckodriver.exe");
+			System.setProperty("webdriver.gecko.driver", getFilePath("drivers", "geckodriver.exe"));
 			driver = new FirefoxDriver();
 		}else {
 			throw new InvalidBrowserNameException();
@@ -69,13 +75,39 @@ public class BaseClass {
 	
 	public void launchBrowser(String browserName, String url, boolean withListener) throws InvalidBrowserNameException {
 		if(browserName.equalsIgnoreCase("chrome")) {
-			System.setProperty("webdriver.chrome.driver", ".\\drivers\\chromedriver.exe");
+			System.setProperty("webdriver.chrome.driver", getFilePath("drivers", "chromedriver.exe"));
 			driver = new ChromeDriver();
 		}else if(browserName.equalsIgnoreCase("firefox")) {
-			System.setProperty("webdriver.gecko.driver", ".\\drivers\\geckodriver.exe");
+			System.setProperty("webdriver.gecko.driver", getFilePath("drivers", "geckodriver.exe"));
 			driver = new FirefoxDriver();
 		}else {
 			throw new InvalidBrowserNameException();
+		}
+		EventFiringWebDriver edriver = new EventFiringWebDriver(driver);
+		EventCapture listener = new EventCapture();
+		edriver.register(listener);
+		driver = edriver;
+		driver.get(url);
+		driver.manage().window().maximize();
+		driver.manage().timeouts().implicitlyWait(10, TimeUnit.SECONDS);
+	}
+	
+	public void launchBrowser(String browserName, String url, boolean withListener, String nodeUrl) {
+		DesiredCapabilities caps = new DesiredCapabilities();
+		caps.setPlatform(Platform.WINDOWS);
+		if(browserName.equalsIgnoreCase("chrome")) {
+			caps = DesiredCapabilities.chrome();
+			caps.setBrowserName(BrowserType.CHROME);
+		}else if(browserName.equalsIgnoreCase("firefox")) {
+			caps = DesiredCapabilities.firefox();
+			caps.setBrowserName(BrowserType.FIREFOX);
+		}else {
+			caps = DesiredCapabilities.internetExplorer();
+		}
+		try {
+			driver = new RemoteWebDriver(new URL(nodeUrl), caps);
+		} catch (MalformedURLException e) {
+			System.out.println(e.getMessage());
 		}
 		EventFiringWebDriver edriver = new EventFiringWebDriver(driver);
 		EventCapture listener = new EventCapture();
